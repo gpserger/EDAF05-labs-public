1837873ca52d4ff0088c9024dea3f8fbbcd39aa1
Gustav Serger <gustav.serger@gmail.com>
2022-03-30 15:08:56 +0200

    Further cleanup

--- 1stablemarriage/lab1.py
+++ 1stablemarriage/lab1.py
@@ -1,16 +1,21 @@
 import sys
 import time
+
+# type .\data\sample\2.in | python3 .\lab1.py
+
 start = time.time()
 
 debug = False
 
 if debug:
+    # read input from specific file
     inp_file = "data/secret/4testhuge.in"
     with open(inp_file, 'r') as f:
         lines = f.read().splitlines()
     n = int(lines[0])
     oneline = [int(item)-1 for sublist in lines[1:] for item in sublist.split(" ")]
 else:
+    # read input from stdin
     lines = []
     for line in sys.stdin:
         lines.append(line.rsplit())
@@ -28,30 +33,40 @@ for i in range(2*n):
     else:
         men[key] = oneline[index+1:index+1+n]
 
-# i:th list member contains the index of the man the ith woman is paired with
-ret = [-1 for _ in range(n)]  # -1 indicates being available
+# i:th list member contains the index of the man the ith woman is paired with, which is the intended ouput format
+result = [-1 for _ in range(n)]  # -1 indicates being available
 
-freemen = [i for i in range(n)]
-while len(freemen) > 0:
-    if debug and len(freemen)%(n/10)==0:
-        print(f"{len(freemen)} men left to pair")
-    # Find index of first free man
-    m = freemen[0]
+if debug:
+    print(f"{time.time()-start}s")
 
-    for w in men[m]: # Go through man's preference in women in order
-        if ret[w] == -1:
-            ret[w] = m
-            freemen.remove(m)
+singlemen = [i for i in range(n)]
+
+# Run while there are single m
+while len(singlemen) > 0:
+    if debug and len(singlemen)%(n / 10)==0:
+        print(f"{len(singlemen)} men left to pair")
+
+    # Find id of first free m
+    m = singlemen[0]
+
+    for w in men[m]:  # Go through m's preference in ws in order
+        if result[w] == -1:
+            # If w is single, pair them up
+            result[w] = m
+            singlemen.remove(m)
             break
-        else: # w is already paired with some other man, m2
-            m2 = ret[w]
+        else:
+            # w is already paired with someone else: m2
+            m2 = result[w]
             if women[w].index(m) < women[w].index(m2):
-                ret[w] = m
-                freemen.remove(m)
-                freemen.append(m2)
+                # If w prefers m over m2, pair them up and make m2 single again
+                result[w] = m
+                singlemen.remove(m)
+                singlemen.append(m2)
                 break
 
-[print(num+1) for num in ret]
+# Everyone is paired so print the result
+[print(num+1) for num in result]
 
 if debug:
     print(f"{time.time()-start}s")
